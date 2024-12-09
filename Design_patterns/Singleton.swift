dc67d412bbfbde81cdf167e3b709f2fc13568a5f
Dinesh Sharma <155711132+DineshSharma1404@users.noreply.github.com>
2024-12-09 08:02:22 +0530

    update factory

--- Design_patterns/Singleton.swift
+++ Design_patterns/Singleton.swift
@@ -5,22 +5,4 @@
 //  Created by Dinesh Sharma on 09/12/24.
 //
 
-import Foundation
 
-class Singleton {
-    
-    static let shared = Singleton()
-    private init() {}
-    
-    
-    func doSomething() {
-        print("Singleton instance is doing something!")
-    }
-}
-
-class SingletonExample {
-    func testSingleton() {
-        let instance = Singleton.shared
-        instance.doSomething()
-    }
-}
